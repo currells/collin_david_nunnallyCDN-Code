ea5701efcd2df8175d8efde9b82088e6b227ba28
collin-nunnally <nunnally.perry@gmail.com>
2024-03-11 12:48:34 -0600

    update
    update

--- CSE-210-main/prepare/Learning03/Program.cs
+++ CSE-210-main/prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionStringCDN());
+        Console.WriteLine(f1.GetDecimalValueCDN());
+
+        Fraction f2 = new Fraction(10);
+        Console.WriteLine(f2.GetFractionStringCDN());
+        Console.WriteLine(f2.GetDecimalValueCDN());
+
+        Fraction f3 = new Fraction(5 , 6);
+        Console.WriteLine(f3.GetFractionStringCDN());
+        Console.WriteLine(f3.GetDecimalValueCDN());
+
+        Fraction f4 = new Fraction(5 , 10);
+        Console.WriteLine(f4.GetFractionStringCDN());
+        Console.WriteLine(f4.GetDecimalValueCDN());
     }
 }
