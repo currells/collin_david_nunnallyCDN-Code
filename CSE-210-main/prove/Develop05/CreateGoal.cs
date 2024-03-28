2307eb006d44adab5a7442727aa240bb2ff2c7e1
collin-nunnally <nunnally.perry@gmail.com>
2024-03-28 16:11:41 -0600

    update

---
+++ CSE-210-main/prove/Develop05/CreateGoal.cs
@@ -0,0 +1,9 @@
+public class CreateGoals
+{
+    public List<string> goals;
+    private string _createGoalCDN;
+    private string _eternalGoalCDN;
+    private string _checklistGoalCDN;
+    private string _simpleGoalCDN;
+    private int _pointsCDN;
+}
