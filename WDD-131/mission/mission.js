3defaa67c2861334c22b4c21ebb010c846d84c1c
collin-nunnally <nunnally.perry@gmail.com>
2025-01-30 21:22:25 -0700

    Updated

--- WDD-131/mission/mission.js
+++ WDD-131/mission/mission.js
@@ -1,18 +1,17 @@
-// Select the dropdown and logo
 const themeselector = document.getElementById("themeselector");
-const logo = document.querySelector(".logo"); // Select the logo image
+const logo = document.querySelector(".logo");
 
 function changeTheme() {
     const theme = themeselector.value;
     
     if (theme === "dark") {
         document.body.classList.add("dark"); 
-        logo.src = "byui-logo_white.png"; // Change to the dark mode logo
+        logo.src = "byui-logo_white.png";
     } else {
         document.body.classList.remove("dark"); 
-        logo.src = "byui-logo_blue.webp"; // Change back to the light mode logo
+        logo.src = "byui-logo_blue.webp";
     }
 }
 
-// Listen for dropdown change
+
 themeselector.addEventListener("change", changeTheme);
