2f1ca022be318b0a346b9ffe834f9454bde0d7d9
collin-nunnally <nunnally.perry@gmail.com>
2025-02-08 22:36:17 -0700

    w05

---
+++ ITM-101/W4-D1/public/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDE6kvKGqBuULhiQiYllp_GnJOwUWZHZZc",
+    authDomain: "nunnally-week4-day1.firebaseapp.com",
+    projectId: "nunnally-week4-day1",
+    storageBucket: "nunnally-week4-day1.firebasestorage.app",
+    messagingSenderId: "870418342852",
+    appId: "1:870418342852:web:02dad4b840595376833a91",
+    measurementId: "G-9JK42XXTW2"
+  };
